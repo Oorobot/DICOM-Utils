0ccc220bd5e6699c2cb0e0d44d70b31ac1681ffb
Oorobot <1069980147@qq.com>
2022-12-07 16:47:58 +0800

    目前最佳分割人体组织的方法

--- main.py
+++ main.py
@@ -34,14 +34,12 @@ fri_xlsx = "D:/admin/Desktop/Data/PETCT-FRI/PET-FRI.xlsx"
 def get_max_component(mask_image: sitk.Image) -> sitk.Image:
     # 得到mask中的多个连通量
     cc_filter = sitk.ConnectedComponentImageFilter()
-    cc_filter.SetNumberOfThreads(8)
     cc_filter.SetFullyConnected(True)
 
     output_image = cc_filter.Execute(mask_image)
 
     # 计算不同连通图的大小
     lss_filter = sitk.LabelShapeStatisticsImageFilter()
-    lss_filter.SetNumberOfThreads(8)
     lss_filter.Execute(output_image)
 
     label_num = cc_filter.GetObjectCount()
@@ -53,41 +51,29 @@ def get_max_component(mask_image: sitk.Image) -> sitk.Image:
         if num > max_num:
             max_label = i
             max_num = num
-    output_array = sitk.GetArrayFromImage(output_image)
-    max_component = (output_array == max_label).astype(np.uint8)
-
-    max_component_image = sitk.GetImageFromArray(max_component)
-    max_component_image.CopyInformation(mask_image)
-
-    return max_component_image
+    return sitk.Equal(output_image, max_label)
 
 
-def get_binary_image(ct_image: sitk.Image, threshold: int = -200) -> sitk.Image:
+def get_binary_image(image: sitk.Image, threshold: int = -200) -> sitk.Image:
     """
     CT threshold = -200, SUVbw threshold = 1e-2
     """
-    ct_array = sitk.GetArrayFromImage(ct_image)
-    binary_ct_array = (ct_array > threshold).astype(np.uint8)
-    binary_ct_image = sitk.GetImageFromArray(binary_ct_array)
-    binary_ct_image.CopyInformation(ct_image)
-    return binary_ct_image
+    return sitk.BinaryThreshold(image, lowerThreshold=threshold, upperThreshold=1e8)
 
 
-def get_binary_morphological_closing(mask_image: sitk.Image):
+def get_binary_morphological_closing(mask_image: sitk.Image, kernel_radius=2):
     bmc_filter = sitk.BinaryMorphologicalClosingImageFilter()
     bmc_filter.SetKernelType(sitk.sitkBall)
-    bmc_filter.SetKernelRadius(3)
+    bmc_filter.SetKernelRadius(kernel_radius)
     bmc_filter.SetForegroundValue(1)
-    bmc_filter.SetNumberOfThreads(8)
     return bmc_filter.Execute(mask_image)
 
 
-def get_binary_morphological_opening(mask_image: sitk.Image):
+def get_binary_morphological_opening(mask_image: sitk.Image, kernel_radius=2):
     bmo_filter = sitk.BinaryMorphologicalOpeningImageFilter()
     bmo_filter.SetKernelType(sitk.sitkBall)
-    bmo_filter.SetKernelRadius(3)
+    bmo_filter.SetKernelRadius(kernel_radius)
     bmo_filter.SetForegroundValue(1)
-    bmo_filter.SetNumberOfThreads(8)
     return bmo_filter.Execute(mask_image)
 
 
@@ -95,7 +81,9 @@ def get_body(
     ct_image: sitk.Image,
     suv_image: sitk.Image,
 ):
-    # CT(去除机床) = CT binary ∩ SUV binary; CT(去除伪影) = CT binary ∪ SUV binary;
+    # 忽略其中进行闭操作和最大连通量
+    # CT(去除机床) = CT binary ∩ SUV binary; CT(去除伪影) = CT binary(去除机床) ∪ SUV binary;
+    # CT(去除伪影) ≈ SUV binary
     ct_binary = get_binary_image(ct_image, -200)
     suv_binary = get_binary_image(suv_image, 3e-2)
 
@@ -103,46 +91,18 @@ def get_body(
     ct_binary_closing_max = get_max_component(
         get_binary_morphological_closing(ct_binary)
     )
-    ct_binary_closing_max_dilate = sitk.BinaryDilate(
-        ct_binary_closing_max,
-        kernelRadius=(2, 3, 3),
-        foregroundValue=1,
-        boundaryToForeground=True,
-    )
-    sitk.WriteImage(ct_binary_closing_max_dilate, "ct_binary_closing_max_dilate.nii.gz")
     # 对SUV进行闭操作，取最大连通量
     suv_binary_closing_max = get_max_component(
         get_binary_morphological_closing(suv_binary)
     )
 
-    # total mask = ct mask ∪ suv mask
-    total_mask = sitk.Or(ct_binary_closing_max, suv_binary_closing_max)
-    # machine mask = ct mask - suv mask
-    machine_mask = sitk.And(
-        ct_binary_closing_max_dilate, sitk.Not(suv_binary_closing_max)
-    )
+    ct_no_machine = sitk.And(ct_binary_closing_max, suv_binary_closing_max)
+    # 取最大连通量
+    ct_no_machine_max = get_max_component(ct_no_machine)
 
-    # 对 machine mask 进行闭包
-    machine_mask_closing = get_binary_morphological_closing(machine_mask)
-
-    # body mask = total mask - machine mask
-    body_mask = sitk.And(total_mask, sitk.Not(machine_mask_closing))
-    # 分别进行闭操作和开操作，然后进行取最大连通量
-    body_mask_closing = get_max_component(get_binary_morphological_closing(body_mask))
-    # body_mask_opening = get_max_component(get_binary_morphological_opening(body_mask))
-
-    # sitk.WriteImage(total_mask, "./Files/total_mask.nii.gz")
-    # sitk.WriteImage(machine_mask, "./Files/machine_mask.nii.gz")
-    # sitk.WriteImage(machine_mask_closing, "./Files/machine_mask_closing.nii.gz")
-    # sitk.WriteImage(body_mask, "./Files/body_mask.nii.gz")
-    # sitk.WriteImage(ct_binary, "./Files/ct_binary.nii.gz")
-    # sitk.WriteImage(ct_binary_closing_max, "./Files/ct_binary_closing_max.nii.gz")
-    # sitk.WriteImage(suv_binary, "./Files/suv_binary.nii.gz")
-    # sitk.WriteImage(suv_binary_closing_max, "./Files/suv_binary_closing_max.nii.gz")
-
-    # sitk.WriteImage(body_mask_closing, "./Files/body_mask_closing.nii.gz")
-    # sitk.WriteImage(body_mask_opening, "./Files/body_mask_opening.nii.gz")
-    return body_mask_closing
+    # 使用超大半径的闭操作，消除伪影
+    ct_no_machine_max_closing = get_binary_morphological_closing(ct_no_machine_max, 20)
+    return ct_no_machine_max_closing
 
 
 def preprosses_based_body_mask(body_mask: np.ndarray):
@@ -165,6 +125,8 @@ def preprosses_based_body_mask(body_mask: np.ndarray):
     return x1_, y1_, x2_, y2_
 
 
+sitk.ProcessObject_SetGlobalDefaultNumberOfThreads(8)
+
 """ 批量计算 body_mask   """
 folders = os.listdir(folder_name)
 for folder in folders:
@@ -181,4 +143,4 @@ for folder in folders:
     print("==> 计算 body mask closing max")
     body_mask = get_body(ct_image, resampled_suv_image)
     print("==> 写入 body mask closing max")
-    sitk.WriteImage(body_mask, f"./Files/body_mask2/{folder}_body_closing_max.nii.gz")
+    sitk.WriteImage(body_mask, f"./Files/body_mask2/{folder}_body_mask.nii.gz")
