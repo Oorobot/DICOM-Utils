cf0c92d11b6208866674fd958a75800f43e15d6c
Oorobot <1069980147@qq.com>
2022-12-13 22:50:04 +0800

    尝试点云可视化显示suv

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,5 +1,24 @@
 import numpy as np
+import SimpleITK as sitk
 import vtk
+from matplotlib.cm import get_cmap
+from vtkmodules.util.vtkImageImportFromArray import vtkImageImportFromArray
+from vtkmodules.vtkCommonCore import vtkPoints, vtkFloatArray, vtkLookupTable
+from vtkmodules.vtkCommonDataModel import (
+    vtkCellArray,
+    vtkPiecewiseFunction,
+    vtkPolyData,
+)
+from vtkmodules.vtkRenderingCore import (
+    vtkActor,
+    vtkPolyDataMapper,
+    vtkColorTransferFunction,
+    vtkVolume,
+    vtkVolumeProperty,
+)
+from vtkmodules.vtkRenderingVolume import vtkFixedPointVolumeRayCastMapper
+
+from utils.dicom import HU2image, SUVbw2image
 
 
 # -----------------------------------------------------------#
@@ -32,7 +51,7 @@ class KeyPressStyle(vtk.vtkInteractorStyleTrackballCamera):
 
 
 # -----------------------------------------------------------#
-#                   索引号 --> 实际坐标位置
+#                   索引号 -. 实际坐标位置
 # -----------------------------------------------------------#
 def index_to_physical_position(boxes, origin, spacing):
     physical_positions = []
@@ -55,3 +74,167 @@ def index_to_physical_position(boxes, origin, spacing):
             ]
         )
     return physical_positions
+
+
+# -----------------------------------------------------------#
+#                      vtk volume
+# -----------------------------------------------------------#
+def ct_vtk_volume(ct_array: np.ndarray, origin: list, spacing: list):
+    ct_array = np.clip(ct_array, -450, 1050)
+    ct_array = sitk.GetArrayFromImage(
+        sitk.GetImageFromArray(ct_array.astype(np.float32))
+    )
+
+    # 创建 vtk 图像
+    vtk_image = vtkImageImportFromArray()
+    vtk_image.SetArray(ct_array)
+    vtk_image.SetDataSpacing(spacing)
+    vtk_image.SetDataOrigin(origin)
+    vtk_image.Update()
+
+    # 体积将通过光线投射alpha合成显示。
+    # 需要光线投射映射器来进行光线投射。
+    volume_mapper = vtkFixedPointVolumeRayCastMapper()
+    volume_mapper.SetInputConnection(vtk_image.GetOutputPort())
+
+    # 使用颜色转换函数，对不同的值设置不同的函数
+    volume_color = vtkColorTransferFunction()
+    cmap = get_cmap("bone")
+    colors = cmap(np.linspace(0, 1, cmap.N))
+    rgb_point = np.linspace(-450, 1050, 256)
+    for i in range(0, 256):
+        volume_color.AddRGBPoint(rgb_point[i], *colors[i, :3])
+
+    # 使用透明度转换函数，用于控制不同组织之间的透明度
+    volume_scalar_opacity = vtkPiecewiseFunction()
+    volume_scalar_opacity.AddPoint(0, 0.00)
+    volume_scalar_opacity.AddPoint(500, 0.20)
+    volume_scalar_opacity.AddPoint(1000, 0.50)
+    volume_scalar_opacity.AddPoint(1150, 0.85)
+
+    # 梯度不透明度函数用于降低体积“平坦”区域的不透明度，同时保持组织类型之间边界的不透明度。梯度是以强度在单位距离上的变化量来测量的
+    volume_gradient_opacity = vtkPiecewiseFunction()
+    volume_gradient_opacity.AddPoint(0, 0.0)
+    volume_gradient_opacity.AddPoint(90, 0.5)
+    volume_gradient_opacity.AddPoint(100, 1.0)
+
+    volume_property = vtkVolumeProperty()
+    volume_property.SetColor(volume_color)
+    volume_property.SetScalarOpacity(volume_scalar_opacity)
+    volume_property.SetGradientOpacity(volume_gradient_opacity)
+    volume_property.SetInterpolationTypeToLinear()
+    volume_property.ShadeOn()
+    volume_property.SetAmbient(0.4)
+    volume_property.SetDiffuse(0.6)
+    volume_property.SetSpecular(0.2)
+
+    volume = vtkVolume()
+    volume.SetMapper(volume_mapper)
+    volume.SetProperty(volume_property)
+
+    return volume
+
+
+def suv_vtk_volume(suv_array: np.ndarray, origin: list, spacing: list):
+    array = SUVbw2image(suv_array, 2.5)
+    suv = sitk.GetArrayFromImage(sitk.GetImageFromArray(array))
+
+    # 创建 vtk 图像
+    vtk_image = vtkImageImportFromArray()
+    vtk_image.SetArray(suv)
+    vtk_image.SetDataSpacing(spacing)
+    vtk_image.SetDataOrigin(origin)
+    vtk_image.Update()
+
+    # 体积将通过光线投射alpha合成显示。
+    # 需要光线投射映射器来进行光线投射。
+    volume_mapper = vtkFixedPointVolumeRayCastMapper()
+    volume_mapper.SetInputConnection(vtk_image.GetOutputPort())
+
+    # 使用颜色转换函数，对不同的值设置不同的函数
+    volume_color = vtkColorTransferFunction()
+    linspace = np.linspace(0, 1, 256)
+    hot_cmap = get_cmap("hot")
+    hot_colors = hot_cmap(linspace)
+    for i in range(0, 256):
+        volume_color.AddRGBPoint(linspace[i], *hot_colors[i, 0:3])
+
+    # 使用透明度转换函数，用于控制不同组织之间的透明度
+    volume_scalar_opacity = vtkPiecewiseFunction()
+    volume_scalar_opacity.AddPoint(0, 0.0)
+    volume_scalar_opacity.AddPoint(0.4, 0.15)
+    volume_scalar_opacity.AddPoint(0.8, 0.15)
+    volume_scalar_opacity.AddPoint(0.9, 0.85)
+
+    # 梯度不透明度函数用于降低体积“平坦”区域的不透明度，同时保持组织类型之间边界的不透明度。梯度是以强度在单位距离上的变化量来测量的
+    volume_gradient_opacity = vtkPiecewiseFunction()
+    volume_gradient_opacity.AddPoint(0, 0)
+    volume_gradient_opacity.AddPoint(0.02, 0.5)
+    volume_gradient_opacity.AddPoint(0.03, 1.0)
+
+    volume_property = vtkVolumeProperty()
+    volume_property.SetColor(volume_color)
+    volume_property.SetScalarOpacity(volume_scalar_opacity)
+    volume_property.SetGradientOpacity(volume_gradient_opacity)
+    volume_property.SetInterpolationTypeToLinear()
+    volume_property.ShadeOn()
+    volume_property.SetAmbient(0.4)
+    volume_property.SetDiffuse(0.6)
+    volume_property.SetSpecular(0.2)
+
+    volume = vtkVolume()
+    volume.SetMapper(volume_mapper)
+    volume.SetProperty(volume_property)
+
+    return volume
+
+
+def suv_point_cloud(suv_array: np.ndarray, origin: list, spacing: list):
+    shape = suv_array.shape
+    suv_image = SUVbw2image(suv_array, 2.5, True)
+
+    hot_cmap = get_cmap("hot")
+    hot_colors = hot_cmap(np.linspace(0, 1, 256))
+
+    points = vtkPoints()
+    lookup = vtkLookupTable()
+    scalars = vtkFloatArray()
+    cells = vtkCellArray()
+
+    total = np.sum((suv_array > 1e-3).astype(np.uint8))
+    lookup.SetNumberOfTableValues(total)
+
+    i = 0
+    for d in range(shape[0]):
+        for h in range(shape[1]):
+            for w in range(shape[2]):
+                if suv_array[d, h, w] <= 1e-3:
+                    continue
+                scalars.InsertNextTuple1(i)
+                x, y, z = np.array([w, h, d]) * np.array(spacing) + np.array(origin)
+                points.InsertNextPoint(x, y, z)
+                cells.InsertNextCell(1)
+                cells.InsertCellPoint(i)
+                lookup.SetTableValue(
+                    i,
+                    *hot_colors[suv_image[d, h, w]],
+                )
+                i = i + 1
+    lookup.Build()
+    polydata = vtkPolyData()
+    polydata.SetPoints(points)
+    polydata.SetVerts(cells)
+    polydata.GetPointData().SetScalars(scalars)
+
+    polygonMapper = vtkPolyDataMapper()
+    polygonMapper.SetInputData(polydata)
+    polygonMapper.SetScalarRange(0, total - 1)
+    polygonMapper.SetLookupTable(lookup)
+    polygonMapper.Update()
+
+    polygonActor = vtkActor()
+    polygonActor.SetMapper(polygonMapper)
+    polygonActor.GetProperty().SetPointSize(1)
+    polygonActor.GetProperty().SetOpacity(0.4)
+
+    return polygonActor
--- visualization.py
+++ visualization.py
@@ -32,7 +32,13 @@ from vtkmodules.vtkRenderingVolume import vtkFixedPointVolumeRayCastMapper
 
 from utils.dicom import HU2image, SUVbw2image
 from utils.utils import rot90
-from utils.visualization import KeyPressStyle, index_to_physical_position
+from utils.visualization import (
+    KeyPressStyle,
+    index_to_physical_position,
+    ct_vtk_volume,
+    suv_vtk_volume,
+    suv_point_cloud,
+)
 
 
 # 读取 CT 或者 SUVbw 文件
@@ -40,34 +46,20 @@ ct_path = "001.nii.gz"
 suv_path = "001_.nii.gz"
 ct_image = sitk.ReadImage(ct_path)
 suv_image = sitk.ReadImage(suv_path)
+origin = [0.0,0.0,0.0]
 spacing = ct_image.GetSpacing()
-
-# 定位框的颜色
-annotation_colors = {"fraction": "Red", "bladder": "Blue", "Other": "Green"}
-# SUVbw color map
-hot_cmap = mpl.cm.get_cmap("hot")
-hot_colors = hot_cmap(np.linspace(0, 1, hot_cmap.N))
-
-# 自定义原点
-origin = (0.0, 0.0, 0.0)
 
 # 图像预处理
 ct_array = sitk.GetArrayFromImage(ct_image)
-ct_array = HU2image(ct_array, 300.0, 1500.0) * 1500.0
-
 suv_array = sitk.GetArrayFromImage(suv_image)
-suv_array[suv_array < 0] = 0
-suv_array = SUVbw2image(suv_array, 10.0, True)
 
+# 定位框的颜色
+annotation_colors = {"fraction": "Red", "bladder": "Blue", "Other": "Green"}
 
 boxes = [[41, 80, 72, 78, 102, 96], [82, 70, 113, 111, 97, 117]]
 box_colors = ["Red", "Blue"]
 
-
-# 翻转和旋转
-array, b = rot90(ct_array, boxes, 0, (0, 2))
-array = sitk.GetArrayFromImage(sitk.GetImageFromArray(array))
-physical_points = index_to_physical_position(b, origin, spacing)
+physical_points = index_to_physical_position(boxes, origin, spacing)
 
 
 # 创建渲染器，渲染窗口和交互工具. 渲染器画入在渲染窗口里，交互工具可以开启基于鼠标和键盘的与场景的交互能力
@@ -84,58 +76,12 @@ iren.SetInteractorStyle(KeyPressStyle())
 # 创建 vtk 颜色
 colors = vtkNamedColors()
 
-# 创建 vtk 图像
-vtk_image = vtkImageImportFromArray()
-vtk_image.SetArray(array)
-vtk_image.SetDataSpacing(spacing)
-vtk_image.SetDataOrigin(origin)
-vtk_image.Update()
-
-
-# 体积将通过光线投射alpha合成显示。
-# 需要光线投射映射器来进行光线投射。
-volume_mapper = vtkFixedPointVolumeRayCastMapper()
-volume_mapper.SetInputConnection(vtk_image.GetOutputPort())
-
-# 使用颜色转换函数，对不同的值设置不同的函数
-volume_color = vtkColorTransferFunction()
-rgb_point = np.linspace(-450, 1050, 256)
-for i in range(0, 256):
-    volume_color.AddRGBPoint(rgb_point[i], i / 255.0, i / 255.0, i / 255.0)
-
-
-# 使用透明度转换函数，用于控制不同组织之间的透明度
-volume_scalar_opacity = vtkPiecewiseFunction()
-volume_scalar_opacity.AddPoint(0, 0.00)
-volume_scalar_opacity.AddPoint(500, 0.15)
-volume_scalar_opacity.AddPoint(1000, 0.15)
-volume_scalar_opacity.AddPoint(1150, 0.85)
-
-
-# 梯度不透明度函数用于降低体积“平坦”区域的不透明度，同时保持组织类型之间边界的不透明度。梯度是以强度在单位距离上的变化量来测量的
-volume_gradient_opacity = vtkPiecewiseFunction()
-volume_gradient_opacity.AddPoint(0, 0.0)
-volume_gradient_opacity.AddPoint(90, 0.5)
-volume_gradient_opacity.AddPoint(100, 1.0)
-
-
-volume_property = vtkVolumeProperty()
-volume_property.SetColor(volume_color)
-volume_property.SetScalarOpacity(volume_scalar_opacity)
-volume_property.SetGradientOpacity(volume_gradient_opacity)
-volume_property.SetInterpolationTypeToLinear()
-volume_property.ShadeOn()
-volume_property.SetAmbient(0.4)
-volume_property.SetDiffuse(0.6)
-volume_property.SetSpecular(0.2)
-
-
-volume = vtkVolume()
-volume.SetMapper(volume_mapper)
-volume.SetProperty(volume_property)
-
-ren.AddViewProp(volume)
-
+ct_volume = ct_vtk_volume(ct_array, [0, 0, 0], spacing)
+ren.AddViewProp(ct_volume)
+# suv_volume = suv_vtk_volume(suv_array, [0, 0, 0], spacing)
+# ren.AddViewProp(suv_volume)
+# suv_pc = suv_point_cloud(suv_array,origin,spacing)
+# ren.AddActor(suv_pc)
 # 添加立方体框
 for p, c in zip(physical_points, box_colors):
     points = vtkPoints()
@@ -184,10 +130,8 @@ for p, c in zip(physical_points, box_colors):
 
 # 设置摄像机
 camera = ren.GetActiveCamera()
-print("volume origin: ", volume.GetOrigin())
-print("volume center: ", volume.GetCenter())
 # print("volume spacing: ", volume.GetSpacing())
-c = volume.GetCenter()
+c = (0,0,0)
 # c = volume.GetOrigin()
 camera.SetViewUp(0, 0, -1)
 camera.SetPosition(c[0], c[1] - 400, c[2])
@@ -196,7 +140,7 @@ camera.Azimuth(30.0)
 camera.Elevation(30.0)
 
 # 设置背景颜色
-ren.SetBackground(colors.GetColor3d("White"))
+ren.SetBackground(colors.GetColor3d("Grey"))
 
 # Increase the size of the render window
 ren_win.SetSize(680, 480)
@@ -205,3 +149,4 @@ ren_win.SetWindowName("CT Visualization")
 # Interact with the data.
 iren.Initialize()
 iren.Start()
+ 
